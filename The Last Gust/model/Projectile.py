d68b4622b1a5bfcea808fda7ceb45d3b700a6d2b
Your Name <dorianfabry@gmail.com>
2020-02-04 14:05:04 +0100

    y

--- The Last Gust/model/Projectile.py
+++ The Last Gust/model/Projectile.py
@@ -1,24 +1,25 @@
-    class Projectile():
+class Projectile:
 
-        def __init(self, nom, degats, velocite, direction, posx, posy):
-            super().__init__();
-            self.degats = degats
-            self.velocite = velocite
-            self.direction = direction
-            self.nom = nom
-            self.rect = self.image_actu.get_rect()
-            self.rect.x = posx
-            self.rect.y = posy
+    def __init(self, nom, degats, velocite, direction, posx, posy):
+        super().__init__()
+        self.degats = degats
+        self.velocite = velocite
+        self.direction = direction
+        self.nom = nom
+        self.image_actu = 0
+        self.rect = self.image_actu.get_rect()
+        self.rect.x = posx
+        self.rect.y = posy
 
-        def deplacement(self):
-            if self.direction = "bas":
-               self.rect.y += self.velocite
-            elif self.direction = "haut":
-                self.rect.y -= self.velocite
-            elif self.direction = "gauche":
-                self.rect.x -= self.velocite
-            else :
-                self.rect.x += self.velocite
+    def deplacement(self):
+        if self.direction ="bas":
+            self.rect.y += self.velocite
+        elif self.direction ="haut":
+            self.rect.y -= self.velocite
+        elif self.direction ="gauche":
+            self.rect.x -= self.velocite
+        else:
+            self.rect.x += self.velocite
 
-        def infligeDegats(self,joueur):
-            joueur.vie -= self.degats
+    def infligedegats(self, joueur):
+        joueur.vie -= self.degats
