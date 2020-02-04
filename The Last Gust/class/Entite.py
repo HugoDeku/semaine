4120b55e0f52e6d7a27514a3ac88529d8e3c802f
HugoDeku <hucureuil@gmail.com>
2020-02-04 09:23:06 +0100

    Entite

--- The Last Gust/class/Entite.py
+++ The Last Gust/class/Entite.py
@@ -4,4 +4,10 @@ class Entite(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        
+
+    def estMort(self):
+        if self.vie <= 0:
+            return True
+
+    def attaque(self):
+        pass
