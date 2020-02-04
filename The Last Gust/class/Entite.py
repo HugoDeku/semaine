70a45dc04f07ae935335289b5b363620b89bcf99
HugoDeku <hucureuil@gmail.com>
2020-02-04 09:30:38 +0100

    modif entite

--- The Last Gust/class/Entite.py
+++ The Last Gust/class/Entite.py
@@ -4,7 +4,9 @@ class Entite(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-
+        self.vie = 1
+        self.velocite = 1
+        self.sprites = {}
     def estMort(self):
         if self.vie <= 0:
             return True
