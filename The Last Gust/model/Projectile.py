b975e31ead76727ee6400305d30a5eeb3ba8832c
Your Name <dorianfabry@gmail.com>
2020-02-04 13:02:38 +0100

    y

--- The Last Gust/model/Projectile.py
+++ The Last Gust/model/Projectile.py
@@ -3,15 +3,15 @@ import pygame
 
     class Projectile(Entite.Entite):
 
-        def __init(self,sprite, degats, velocite,direction ):
-             super().__init__();
-             self.degats = degats
-             self.velocite = velocite
-             self.direction = direction
-             self.sprite = sprite
-             self.rect = self.image_actu.get_rect()
-             self.rect.x = posx
-             self.rect.y = posy
+        def __init(self, sprite, degats, velocite, direction, posx, posy):
+            super().__init__();
+            self.degats = degats
+            self.velocite = velocite
+            self.direction = direction
+            self.sprite = sprite
+            self.rect = self.image_actu.get_rect()
+            self.rect.x = posx
+            self.rect.y = posy
 
         def deplacement(self):
             if self.direction = "bas":
