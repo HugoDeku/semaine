72c4843b3991c55ca6f43747083cae720c10b008
Padir2 <locatelliraymond@gmail.com>
2020-02-04 11:07:56 +0100

    Billy

---
+++ The Last Gust/model/Joueur.py
@@ -0,0 +1,10 @@
+from Entite import *
+
+class Joueur(Entite):
+
+    def __init__(self):
+        super().__init__()
+        self.nom="Billy"
+        self.vie=12
+        self.velocite=12
+        self.p
